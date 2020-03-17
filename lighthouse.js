4399ebfedab6205dd2392d45178ac293767cc57c
Hudson <Simon.Hudson@experian.com>
2020-03-17 12:33:23 +0000

    generate report

--- lighthouse.js
+++ lighthouse.js
@@ -3,6 +3,8 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 const argv = require('yargs').argv;
+const url = require('url');
+const fs = require('fs');
 
 const envUrl = {
     'local': 'https://localhost:8080',
@@ -18,7 +20,12 @@ const launchChromeAndRunLighthouse = url => {
             port: chrome.port
         };
         return lighthouse(url, opts).then(results => {
-            return chrome.kill().then(() => results.report);
+            return chrome.kill().then(() => {
+                return {
+                    js: results.lhr,
+                    json: results.report
+                };
+            });
         });
     });
 };
@@ -28,8 +35,20 @@ if (!argv.env) {
 } else {
     const url = envUrl[argv.env];
     if (url) {
+        const urlObj = new URL(url);
+        const reportsRootDir = 'reports';
+        if (!fs.existsSync(reportsRootDir)) fs.mkdirSync(reportsRootDir);
+        let dirName = `${reportsRootDir}/${urlObj.host.replace('www.','')}`;
+        if (urlObj.pathname !== '/') dirName = dirName + urlObj.pathname.replace(/\//g, '_');
+        if (!fs.existsSync(dirName)) fs.mkdirSync(dirName);
         launchChromeAndRunLighthouse(url).then(results => {
-            console.log(results);
+            fs.writeFile(
+                `${dirName}/${results.js['fetchTime'].replace(/:/g, '_')}.json`,
+                results.json,
+                err => {
+                  if (err) throw err;
+                }
+              );
         });
     } else {
         console.log(`No environment URL found for ${argv.env}`)
